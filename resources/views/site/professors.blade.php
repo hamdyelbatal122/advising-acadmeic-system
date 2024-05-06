5c6e4c0475146a61be41896cb94b90c56c1bdfa2
hamdyelbatal122 <elbatalhamdy77@gmail.com>
2024-05-06 15:54:43 +0300

    chore: Update advising page content

--- resources/views/site/professors.blade.php
+++ resources/views/site/professors.blade.php
@@ -1,10 +1,10 @@
 @extends('site.layouts.main')
-@section('title', 'Professor')
+@section('title', 'Professors')
 @section('content')
 <section class="teacher-part">
    <div class="container">
       <div class="teacher-content">
-         <h1 class="section-page-title">Our Professor</h1>
+         <h1 class="section-page-title">Our Professors</h1>
          <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4">
             <div class="col">
                <div class="teacher-card">
@@ -28,7 +28,7 @@
                      <img class="teacher-avater" src=" {{asset('assets/images/site/professors/e2022faeb5e9a39dd4d0b4afea245c5d96f2a3a4a4a28d426f51359d555fad6195c9322f140e50d9bac81455c3ac3bdf6ed124cea4b17ddc498278b28da91fd1.jpg')}}" alt="Alisha Henry">
                      <div class="teacher-overlay">
                         <a target="_blank" href="https://www.facebook.com" class="lab-fill-facebook-round"></a>
-                        <a target="_blank" href="https://twitter.com/" class="lab-fill-twitter-round"></a>
+                        <a target="_blank" href="" class="lab-fill-twitter-round"></a>
                         <a target="_blank" href="https://www.linkedin.com/" class="lab-fill-linkedin-round"></a>
                         <a target="_blank" href="https://www.google.com/" class="lab-fill-google-plus-round"></a>
                      </div>
@@ -44,9 +44,9 @@
                   <div class="teacher-media">
                      <img class="teacher-avater" src=" {{asset('assets/images/site/professors/4fd8023c6909662e7ec6a2ff44ceece5f071eab35eac6f1654598c9583292a89b814bcd81a0b749261c8b252d75dd9dfa5c84b9ff34de988d4f045bba12d7be7.jpg')}}" alt="Archie Webb">
                      <div class="teacher-overlay">
-                        <a target="_blank" href="https://www.facebook.com/inilabs" class="lab-fill-facebook-round"></a>
-                        <a target="_blank" href="https://twitter.com/" class="lab-fill-twitter-round"></a>
-                        <a target="_blank" href="https://www.linkedin.com/company/14437981" class="lab-fill-linkedin-round"></a>
+                        <a target="_blank" href="" class="lab-fill-facebook-round"></a>
+                        <a target="_blank" href="" class="lab-fill-twitter-round"></a>
+                        <a target="_blank" href="" class="lab-fill-linkedin-round"></a>
                      </div>
                   </div>
                   <div class="teacher-meta">
@@ -60,9 +60,9 @@
                   <div class="teacher-media">
                      <img class="teacher-avater" src=" {{asset('assets/images/site/professors/d3adf6ba8f39b4c35c1611e1db89e9e766d82bf599563d19d6b41e6da53843ecc69c6376a625063b9628bf4bd3c3d2a3e06b42d74e1509564a83d6e2e70990cd.jpg')}}" alt="Cooper Donin">
                      <div class="teacher-overlay">
-                        <a target="_blank" href="https://www.facebook.com/inilabs" class="lab-fill-facebook-round"></a>
-                        <a target="_blank" href="https://twitter.com/" class="lab-fill-twitter-round"></a>
-                        <a target="_blank" href="https://www.linkedin.com/company/14437981" class="lab-fill-linkedin-round"></a>
+                        <a target="_blank" href="" class="lab-fill-facebook-round"></a>
+                        <a target="_blank" href="" class="lab-fill-twitter-round"></a>
+                        <a target="_blank" href="" class="lab-fill-linkedin-round"></a>
                      </div>
                   </div>
                   <div class="teacher-meta">
@@ -76,9 +76,9 @@
                   <div class="teacher-media">
                      <img class="teacher-avater" src=" {{asset('assets/images/site/professors/36c89fa3ddd8fc3950f0cb87fe6bb0cb067a7d519636442cbfa3a59d0181f9aa03d549d362e6ebb22bcd53029d36e9cd687d8ce23bb5bd60354a94b26fc8d267.jpg')}}" alt="Georgina Hayward">
                      <div class="teacher-overlay">
-                        <a target="_blank" href="https://www.facebook.com/inilabs" class="lab-fill-facebook-round"></a>
-                        <a target="_blank" href="https://twitter.com/" class="lab-fill-twitter-round"></a>
-                        <a target="_blank" href="https://www.linkedin.com/company/14437981" class="lab-fill-linkedin-round"></a>
+                        <a target="_blank" href="" class="lab-fill-facebook-round"></a>
+                        <a target="_blank" href="" class="lab-fill-twitter-round"></a>
+                        <a target="_blank" href="" class="lab-fill-linkedin-round"></a>
                      </div>
                   </div>
                   <div class="teacher-meta">
@@ -105,9 +105,9 @@
                   <div class="teacher-media">
                      <img class="teacher-avater" src=" {{asset('assets/images/site/professors/b02144ecf26d8970cf369d73cfee3e5f922c0f52a5fad08f380a32899c3ac6ae5cfd389ed701e869c16a5139c80d1621bf95b96df2afb57f0dd29b4050475a40.jpg')}}" alt="Jordan Mitchell">
                      <div class="teacher-overlay">
-                        <a target="_blank" href="https://www.facebook.com/inilabs" class="lab-fill-facebook-round"></a>
-                        <a target="_blank" href="https://twitter.com/" class="lab-fill-twitter-round"></a>
-                        <a target="_blank" href="https://www.linkedin.com/company/14437981" class="lab-fill-linkedin-round"></a>
+                        <a target="_blank" href="" class="lab-fill-facebook-round"></a>
+                        <a target="_blank" href="" class="lab-fill-twitter-round"></a>
+                        <a target="_blank" href="" class="lab-fill-linkedin-round"></a>
                      </div>
                   </div>
                   <div class="teacher-meta">
@@ -121,9 +121,9 @@
                   <div class="teacher-media">
                      <img class="teacher-avater" src=" {{asset('assets/images/site/professors/342fc281e3c33143de90484dd05978ab1cf77319c251d0f0747f7aa1d628c9e0a7322d9580e95585c074c78c01fcad9af7fb4504190cd37dfa71aae54d4ae208.jpg')}}" alt="Katie Walsh">
                      <div class="teacher-overlay">
-                        <a target="_blank" href="https://www.facebook.com/inilabs" class="lab-fill-facebook-round"></a>
-                        <a target="_blank" href="https://twitter.com/" class="lab-fill-twitter-round"></a>
-                        <a target="_blank" href="https://www.linkedin.com/company/14437981" class="lab-fill-linkedin-round"></a>
+                        <a target="_blank" href="" class="lab-fill-facebook-round"></a>
+                        <a target="_blank" href="" class="lab-fill-twitter-round"></a>
+                        <a target="_blank" href="" class="lab-fill-linkedin-round"></a>
                      </div>
                   </div>
                   <div class="teacher-meta">
@@ -137,8 +137,8 @@
                   <div class="teacher-media">
                      <img class="teacher-avater" src=" {{asset('assets/images/site/professors/c4eedb34d37cb7044390aef0e0d43ae27385ff11ecfe22131c1256f6518f1b4b642ba647e65a1bfc1e7e58585b2bad3bc28e2f0663bd6f643cd39da9ff7e4354.jpg')}}" alt="Maisie Pollard">
                      <div class="teacher-overlay">
-                        <a target="_blank" href="https://www.facebook.com/inilabs" class="lab-fill-facebook-round"></a>
-                        <a target="_blank" href="https://twitter.com/" class="lab-fill-twitter-round"></a>
+                        <a target="_blank" href="" class="lab-fill-facebook-round"></a>
+                        <a target="_blank" href="" class="lab-fill-twitter-round"></a>
                      </div>
                   </div>
                   <div class="teacher-meta">
@@ -152,9 +152,9 @@
                   <div class="teacher-media">
                      <img class="teacher-avater" src=" {{asset('assets/images/site/professors/a08ddda4afa139e3bdca1a2887141fbaeb91f469b67c68a95b2656f9888574176d0a1fe8ff5f243ce889a4895e064edcfbfb9997b3074cec77107802426d0593.jpg')}}" alt="Mia O'Donnell">
                      <div class="teacher-overlay">
-                        <a target="_blank" href="https://www.facebook.com/inilabs" class="lab-fill-facebook-round"></a>
-                        <a target="_blank" href="https://twitter.com/" class="lab-fill-twitter-round"></a>
-                        <a target="_blank" href="https://www.linkedin.com/company/14437981" class="lab-fill-linkedin-round"></a>
+                        <a target="_blank" href="" class="lab-fill-facebook-round"></a>
+                        <a target="_blank" href="" class="lab-fill-twitter-round"></a>
+                        <a target="_blank" href="" class="lab-fill-linkedin-round"></a>
                      </div>
                   </div>
                   <div class="teacher-meta">
@@ -168,9 +168,9 @@
                   <div class="teacher-media">
                      <img class="teacher-avater" src=" {{asset('assets/images/site/professors/b3498952e9b0d1f451e05a83993fd3d5ee4dd9ddd3b9616d19099a98b349ac8c9959e894189f69c25b53cfb177a0ec36e2067b9a10f287ac984e97532c2dddaf.jpg')}}" alt="Naomi Doyle">
                      <div class="teacher-overlay">
-                        <a target="_blank" href="https://www.facebook.com/inilabs" class="lab-fill-facebook-round"></a>
-                        <a target="_blank" href="https://twitter.com/" class="lab-fill-twitter-round"></a>
-                        <a target="_blank" href="https://www.linkedin.com/company/14437981" class="lab-fill-linkedin-round"></a>
+                        <a target="_blank" href="" class="lab-fill-facebook-round"></a>
+                        <a target="_blank" href="" class="lab-fill-twitter-round"></a>
+                        <a target="_blank" href="" class="lab-fill-linkedin-round"></a>
                      </div>
                   </div>
                   <div class="teacher-meta">
@@ -184,9 +184,9 @@
                   <div class="teacher-media">
                      <img class="teacher-avater" src=" {{asset('assets/images/site/professors/8cd47e4b6eb374e657c32c78b149ae0e53104e7bcd0b3781329ffb7655cefcee81d995466c5ac7b959a7172deea016f20e4a7a9f098110f66b9977485cf75748.jpg')}}" alt="Riley Reynolds">
                      <div class="teacher-overlay">
-                        <a target="_blank" href="https://www.facebook.com/inilabs" class="lab-fill-facebook-round"></a>
-                        <a target="_blank" href="https://twitter.com/" class="lab-fill-twitter-round"></a>
-                        <a target="_blank" href="https://www.linkedin.com/company/14437981" class="lab-fill-linkedin-round"></a>
+                        <a target="_blank" href="" class="lab-fill-facebook-round"></a>
+                        <a target="_blank" href="" class="lab-fill-twitter-round"></a>
+                        <a target="_blank" href="" class="lab-fill-linkedin-round"></a>
                      </div>
                   </div>
                   <div class="teacher-meta">
